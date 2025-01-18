29912d3b77a6be20fa3bfc2dbb52ce424e8947c4
zachary.lauzon <zachary.lauzon@fdmgroup.com>
2025-01-18 07:19:13 -0600

    fix errors in fibonacci file

--- scripts/fibonacci.sh
+++ scripts/fibonacci.sh
@@ -4,11 +4,11 @@ N=${1:-10}
 a=0
 b=1
 
-echo "The fibonacci series is : "
+echo "The Fibonacci series is : "
 
-for ((i=0; i<N; i++))
+for (( i=0; i<N; i++ ))
 do
-    echo -e "$i/t$a"
+    echo -e "$i\t$a"
     fn=$((a + b))
     a=$b
     b=$fn
